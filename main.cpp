53fe7c75acf8d52721b25bf2d03b315f26e85074
5003385 <91500260+5003385@users.noreply.github.com>
2021-12-11 11:57:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+int main() {
+
+    int n;
+
+    cin >> n;
+
+    float array[n];
+
+    for (int i = 0; i < n; ++i) {
+
+        cin >> array[i];
+    }
+
+    float res= max (array, n);
+    cout << res << endl;
+
+
+    return 0;
 }
