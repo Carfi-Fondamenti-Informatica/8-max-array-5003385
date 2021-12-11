ee0e1060944606b27143cb9f3db35834b5d97e38
5003385 <91500260+5003385@users.noreply.github.com>
2021-12-11 11:57:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,25 @@
 
+#include "lib.h"
+
+float max(float array[], int n){
+
+    float max=0;
+
+    if (n=1){
+        max=array[0];
+    }
+
+    for (int i=1; i<n-1; i++){
+        if (array[i-1]>=array[i]){
+            max=array[i-1];
+        } else {
+            if(array[i]>=array[i+1]){
+                max=array[i];
+            } else {
+                max=array[i+1];
+            }
+        }
+    }
+
+    return max;
+}
